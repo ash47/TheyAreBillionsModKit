70cb05300df686428ff2ccf84f1e59910ff6b52d
Ashley Schmid <ash.fourtyseven@gmail.com>
2017-12-24 02:12:30 +1100

    added method injector

---
+++ MethodInjector/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodInjector
+{
+    class Program
+    {
+        // The name of the executable to inject into
+        private static string theExecutable = "TheyAreBillions.exe";
+
+        private static Assembly theyAreBillionsAssembly;
+
+        // Called when the program is launched
+        static void Main(string[] args)
+        {
+            LogMessage("Injector was started!");
+
+            // Attempt to load the assembly
+            theyAreBillionsAssembly = Assembly.LoadFile(Path.GetFullPath(theExecutable));
+            if(theyAreBillionsAssembly == null)
+            {
+                LogMessage("Failed to load assembly, ensure this executable is run from the folder containing: " + theExecutable);
+                return;
+            }
+
+            // Grab ZXProgram
+            Type ZXProgram = theyAreBillionsAssembly.GetType("ZX.Program");
+            if(ZXProgram == null)
+            {
+                LogMessage("Failed to load main program class :/");
+                return;
+            }
+
+            /*
+                Perform patching
+            */
+
+            // Buildings dont cost anything to build
+            ReplaceMethod("ZX.ZXLevelState", "CanPayResources", BindingFlags.Instance | BindingFlags.Public);
+            ReplaceMethod("ZX.ZXLevelState", "PayResources", BindingFlags.Instance | BindingFlags.Public);
+
+            // Allow hacked save games to be loaded
+            ReplaceMethod("ZX.ZXGame", "CheckSaveGame", BindingFlags.Static | BindingFlags.NonPublic);
+
+            // Enable dev tools & private stuff
+            ReplaceMethod("ZX.ZXGame", "get_IsDevelopmentVersion", BindingFlags.Static | BindingFlags.Public);
+            ReplaceMethod("ZX.ZXGame", "get_IsBetaPrivateVersion", BindingFlags.Static | BindingFlags.Public);
+            
+            // Get a reference to the Main method
+            MethodInfo main = ZXProgram.GetMethod("Main", BindingFlags.Static | BindingFlags.NonPublic);
+            if(main == null)
+            {
+                LogMessage("Failed to find the main method :/");
+                return;
+            }
+            
+            // Attempt to Invoke it
+            try
+            {
+                main.Invoke(ZXProgram, new object[]
+                {
+                    args
+                });
+            }
+            catch(Exception e)
+            {
+                LogMessage("Failed to start the game :/");
+                LogMessage(e.Message);
+                LogMessage(e.StackTrace);
+            }
+            
+        }
+
+        // Replaces a method with one defined below
+        public static void ReplaceMethod(string className, string methodName, BindingFlags attr)
+        {
+            // Grab the method we will replace
+            Type theType = theyAreBillionsAssembly.GetType(className);
+            MethodInfo targetMethod = theType.GetMethod(methodName, attr);
+            if(targetMethod == null)
+            {
+                LogMessage("Failed to find method: " + className + " :: " + methodName);
+                return;
+            }
+
+            // Grab the method we will inject
+            MethodInfo newMethod = typeof(MethodInjector.Program).GetMethod(methodName, attr);
+            if (newMethod == null)
+            {
+                LogMessage("Failed to find replacement method: " + className + " :: " + methodName);
+                return;
+            }
+
+            // Prepare methods
+            RuntimeHelpers.PrepareMethod(targetMethod.MethodHandle);
+            RuntimeHelpers.PrepareMethod(newMethod.MethodHandle);
+
+            unsafe
+            {
+                if (IntPtr.Size == 4)
+                {
+                    int* inj = (int*)newMethod.MethodHandle.Value.ToPointer() + 2;
+                    int* tar = (int*)targetMethod.MethodHandle.Value.ToPointer() + 2;
+
+                    #if DEBUG
+                        //Console.WriteLine("\nVersion x86 Debug\n");
+
+                        byte* injInst = (byte*)*inj;
+                        byte* tarInst = (byte*)*tar;
+
+                        int* injSrc = (int*)(injInst + 1);
+                        int* tarSrc = (int*)(tarInst + 1);
+
+                        *tarSrc = (((int)injInst + 5) + *injSrc) - ((int)tarInst + 5);
+                    #else
+                        //Console.WriteLine("\nVersion x86 Release\n");
+                        *tar = *inj;
+                    #endif
+                }
+                else
+                {
+
+                    long* inj = (long*)newMethod.MethodHandle.Value.ToPointer() + 1;
+                    long* tar = (long*)targetMethod.MethodHandle.Value.ToPointer() + 1;
+                    #if DEBUG
+                        //Console.WriteLine("\nVersion x64 Debug\n");
+                        byte* injInst = (byte*)*inj;
+                        byte* tarInst = (byte*)*tar;
+
+
+                        int* injSrc = (int*)(injInst + 1);
+                        int* tarSrc = (int*)(tarInst + 1);
+
+                        *tarSrc = (((int)injInst + 5) + *injSrc) - ((int)tarInst + 5);
+                    #else
+                        //Console.WriteLine("\nVersion x64 Release\n");
+                        *tar = *inj;
+                    #endif
+                }
+            }
+
+            LogMessage("Successfully replaced: " + className + " :: " + methodName);
+        }
+
+        // Replacement for can pay resources
+        public bool CanPayResources(object ignoreThis)
+        {
+            return true;
+        }
+
+        // Replacement for pay resources
+        public void PayResources(object ignoreThis)
+        {
+            // Do nothing
+            return;
+        }
+
+        // Replacement for check save files
+        private static bool CheckSaveGame(string file)
+        {
+            return true;
+        }
+
+        // Returns that this is a dev build
+        public static bool get_IsDevelopmentVersion()
+        {
+            return true;
+        }
+
+        // Returns that this is a private build
+        public static bool get_IsBetaPrivateVersion()
+        {
+            return true;
+        }
+
+        private static void LogMessage(string error)
+        {
+            System.IO.File.AppendAllText("inject.log", "error: " + error + Environment.NewLine);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # TheyAreBillionsModKit
 Tools &amp; Research notes for modding "They Are Billions"
+
+# What is this?
+
+ - This is a set of tools and notes for creating mods for "They Are Billions". This project must not be used for illegal purposes, or for getting a highscore on any leaderboards that are present in the game, it is strictly for educational purposes and for people to experiment with.
+
+# Tools
+
+## MethodInjector
+
+ - This tool is used to inject custom code into the memory of the game without modifying the actual executable itself.
+ - If you are using this, you need to build RELEASE x64, or it WILL FAIL TO INJECT!
+
+### Current Patches
+
+ - Allow loading of hacked save games -- This simply turns of the save game checksum code that is being used.
+ - Building costs no money -- This removes the cost of buildings, and allows you to build even if you have negative resources.
+ - Enable DEV and PRIVATE modes.
+
+### Cheats
+ - Enable cheats by pressing `control` + `shift` + `H`
+
+ +			Speed Up Time
+ -			Slow Down Time
+ CTR + F	Toggle Fog
+ CTR + R	Grant Resources
+ CTR + L	Toggle God Mode (Damage on / off)
+ ALT + R	Toggle Resource Grid
+ CTR + F9	Grant All Research
+ F9			Activate Cinematic Mode
+ ALT + F12	Instant Victory
+ CTR + P	???
+
+
+# Notes
+
+## Enabling Development / Private Beta Tools
+ - ZX.Program
+   - ZXGame.IsDevelopmentVersion = true;
+   - ZXGame.IsBetaPrivateVersion = true;
+
+## Enabling Cheats
+ - ZX.ZXSystem_GameLevel
+   - OnKeyUp
+     - Invert Logic: ZXGame.CheatsEnabled
+
+## Allow loading of modifed save files
+ - ZX.ZXGame
+   - CheckSaveGame
+     - Remove check from here
+
+## Infinite Resources
+ - ZX.ZXLevelState
+   - CanPayResources
+   - PayResources
+   		- Remove / Change logic
+
