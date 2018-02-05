31593f236b11b29aab755f21e2ff102fe5b50336
Ashley Schmid <ash.fourtyseven@gmail.com>
2018-02-05 20:59:30 +1100

    added nodejs module installer

---
+++ DataEditor/install_modules.bat
@@ -0,0 +1,13 @@
+@echo off
+
+echo Installing modules using NPM...
+
+cmd /c npm install
+
+echo You should now have a "node_modules folder".
+
+echo If you don't have a "node_modules" folder, please ensure you install NodeJS and add NodeJS to your path (via the installation option).
+
+echo https://nodejs.org/en/
+
+pause
